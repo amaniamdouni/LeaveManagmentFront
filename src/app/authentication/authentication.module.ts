27937c8a1c26b1c8bfa4c91db8a4da2791013e54
AbidiFarah <labidifarah090@gmail.com>
2023-06-19 15:25:50 +0200

    commit-login

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,7 +5,6 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { Page500Component } from './page500/page500.component';
 import { Page404Component } from './page404/page404.component';
 import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
 import { LockedComponent } from './locked/locked.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -15,7 +14,6 @@ import { SharedModule } from '../shared/shared.module';
     Page500Component,
     Page404Component,
     SigninComponent,
-    SignupComponent,
     LockedComponent,
     ForgotPasswordComponent,
   ],
